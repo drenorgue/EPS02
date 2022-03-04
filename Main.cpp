769c6efe4371fbaca6164e02b1696cd5dc36b5c4
drenorgue <71455343+drenorgue@users.noreply.github.com>
2022-03-04 18:38:07 +0100

    a
    a

--- Main.cpp
+++ Main.cpp
@@ -2,42 +2,52 @@
 
 using namespace std;
 
+// il faut les points de maitrise (uniquement la réussite pour les deux premières course et aussi l'allure pour les 2 dernières) 
+// Performance vma moyenne selon garçon / fille
+
+
+
 int main() {
 	int NombreEleve;
 	cout << "Nombre d'eleve : ";
 	cin >> NombreEleve;
+	string nomDeEleve[100];
+	int vma[100];
+	int PointObserv[100];
+	int NombreDePlotCourse1[100];
+	int NombreDePlotCourse2[100];
+	int note[100];
+	bool Sexe[100];
 
-	
 
 	for (int i = 1; i <= NombreEleve; i++)
 	{
-		string nomDeEleve[100];
 		cout << "prenom de l'eleve : ";
 		cin >> nomDeEleve[i];
 		cout << endl;
 
-		int vma[100];
+		
 		cout << "VMA de l'eleve : ";
 		cin >> vma[i];
 		cout << endl;
+		
 
-
-		int PointObserv [100];
+		
 		cout << "Nombre de point d'observation : ";
 		cin >> PointObserv[i];
 		cout << endl;
 
-		int NombreDePlotCourse1 [100];
+		
 		cout << "Nombre de plot course 1 : ";
 		cin >> NombreDePlotCourse1[i];
 		cout << endl;
 
-		int NombreDePlotCourse2[100];
+		
 		cout << "Nombre de plot de course 2 : ";
 		cin >> NombreDePlotCourse2[i];
 		cout << endl;
 
-		int note[100];
+		
 
 	}
 
